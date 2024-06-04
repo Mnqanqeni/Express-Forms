daee19480ba712964ff6a2a8d4d952e4f48f850b
Mnqanqeni <ndiyakholwa.mnqanqeni@umuzi.org>
2024-06-04 20:59:48 +0200

    add helper

--- src/script.js
+++ src/script.js
@@ -1,112 +1,85 @@
-const {pool}=require("./config");
+const { pool } = require("./config");
+const { validateName, validateAge, validateDate, validateTime, validateAssistant, validateComments, validateId } = require("./script_helper");
+
+const {queries} = require("./script_object");
 
 const createVisitorsTable = async () => {
-    const query = `
-      CREATE TABLE IF NOT EXISTS Visitors (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        age INT NOT NULL,
-        date DATE NOT NULL,
-        time TIME NOT NULL,
-        assistant VARCHAR(100) NOT NULL,
-        comments TEXT
-      );
-    `;
-    await pool.query(query);
-  return "table created";
+    await pool.query(queries.createVisitorsTable);
+    return "table created";
 };
-
-// validate all the inputs, for date make sure that you usd the date format YYYY-MM-DD method
-// comments must be more that one letter
-// the rest is normal validation.
 
 const addNewVisitor = async (visitor) => {
     const { name, age, date, time, assistant, comments } = visitor;
-    const query = `
-      INSERT INTO Visitors (name, age, date, time, assistant, comments)
-      VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING id;
-    `;
+    
+    validateName(name);
+    validateAge(age);
+    validateDate(date);
+    validateTime(time);
+    validateAssistant(assistant);
+    validateComments(comments);
+    
     const values = [name, age, date, time, assistant, comments];
-    const result = await pool.query(query, values);
+    const result = await pool.query(queries.addNewVisitor, values);
     return result.rows[0].id;
-
 };
 
-
 const listAllVisitors = async () => {
-    const query = `
-      SELECT id, name FROM Visitors;
-    `;
-    const result = await pool.query(query);
+    const result = await pool.query(queries.listAllVisitors);
     return result.rows;
- 
 };
 
 const deleteVisitor = async (visitorId) => {
-    const query = `
-      DELETE FROM Visitors
-      WHERE id = $1;
-    `;
-    const result = await pool.query(query, [visitorId]);
+    validateId(visitorId);
+
+    const result = await pool.query(queries.deleteVisitor, [visitorId]);
     return "visitor deleted";
 };
 
 const updateVisitor = async (visitorId, columnKey, newValue) => {
-    const query = `
-      UPDATE Visitors
-      SET ${columnKey} = $1 WHERE id = $2
-    `;
+    validateId(visitorId);
+    
+    switch (columnKey) {
+        case "name": validateName(newValue); break;
+        case "age": validateAge(newValue); break;
+        case "date": validateDate(newValue); break;
+        case "time": validateTime(newValue); break;
+        case "assistant": validateAssistant(newValue); break;
+        case "comments": validateComments(newValue); break;
+        default: throw new Error("Invalid column key");
+    }
+    const query = queries.updateVisitor.replace('$1', columnKey);
     const values = [newValue, visitorId];
     const result = await pool.query(query, values);
     return "visitor updated";
-  
 };
 
-
 const viewOneVisitor = async (visitorId) => {
-    const query = `
-      SELECT * FROM Visitors
-      WHERE id = $1;
-    `;
-    const result = await pool.query(query, [visitorId]);
+    validateId(visitorId);
+
+    const result = await pool.query(queries.viewOneVisitor, [visitorId]);
     result.rows[0].date = result.rows[0].date.toISOString().split("T")[0];
     return result.rows[0];
 };
 
 const deleteAllVisitors = async () => {
-    const query = `
-      DELETE FROM Visitors;
-    `;
-    const result = await pool.query(query);
+    const result = await pool.query(queries.deleteAllVisitors);
     return "all visitors deleted";
 };
 
 const viewLastVisitor = async () => {
-    const query = `
-      SELECT id FROM Visitors
-      ORDER BY id DESC
-      LIMIT 1;
-    `;
-    const result = await pool.query(query);
-    return result.rows;
+    const result = await pool.query(queries.viewLastVisitor);
+    return result.rows[0];
 };
-// addNewVisitor({
-//   name: "Siya Seya",
-//   age: 25,
-//   date: "2022-01-01",
-//   time: "12:00:00",
-//   assistant: "Jane Doe",
-//   comments: "This is a comment",})
 
 deleteAllVisitors().then((data) => console.log(data));
+
 module.exports = {
-  createVisitorsTable,
-  addNewVisitor,
-  listAllVisitors,
-  deleteVisitor,
-  updateVisitor,
-  viewOneVisitor,
-  deleteAllVisitors,
-  viewLastVisitor
+    createVisitorsTable,
+    addNewVisitor,
+    listAllVisitors,
+    deleteVisitor,
+    updateVisitor,
+    viewOneVisitor,
+    deleteAllVisitors,
+    viewLastVisitor
 };
