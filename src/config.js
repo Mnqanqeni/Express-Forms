53d3ece066edb1430a52575d38e5d36deee8c476
Mnqanqeni <ndiyakholwa.mnqanqeni@umuzi.org>
2024-07-18 13:35:12 +0200

    Changes Addressd

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ dotenv.config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER || user,
-  host: process.env.POSTGRES_HOST || localhost,
-  database: process.env.POSTGRES_DB || umuzi_db,
+  user: process.env.POSTGRES_USER || "user",
+  host: process.env.POSTGRES_HOST || "localhost",
+  database: process.env.POSTGRES_DB || "db",
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT || 5432,
 });
