a98009c7e98afe789f54c0456591a83467aaa724
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2024-09-26 18:27:37 +0200

    pushes

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const {addNewVisitor,createVisitorsTable} = require("./src/script");
+
 const app = express()
-app.get("/",(req,res)=>{
-    res.send("Home page")
-})
-app.listen(5000,()=>{
-    console.log("Server is listening on port 5000.....")
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/new_visitor",express.static("public"));
+
+app.get("/submit", async (req, res) => {
+    try {
+        let { name, assistant, age, date, time, comments } = req.query;
+        console.log(req.query)
+        age = parseInt(age);
+
+    
+        const result = await createVisitorsTable();
+        console.log(result)
+        const results = await addNewVisitor({ name, assistant, age, date, time, comments });
+        console.log(results)
+        res.status(201).json({ message: "Visitor added successfully", results });
+    } catch (error) {
+
+        console.error(error);
+        res.status(400).json({ error: error.message });
+    }
+});
+
+
+app.listen(5001,()=>{
+    console.log("Server is listening on port 5001.....");
 })
