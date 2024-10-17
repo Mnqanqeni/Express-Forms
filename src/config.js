da7a52c5d807d4b3e30b028a10ab27d69cbbde3b
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2024-10-17 12:36:45 +0200

    Changes Addressed

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER || "user",
   host: process.env.POSTGRES_HOST || "localhost",
   database: process.env.POSTGRES_DB || "db",
-  password: process.env.POSTGRES_PASSWORD || "pass",
+  password: process.env.POSTGRES_PASSWORD ,
   port: process.env.POSTGRES_PORT || 5432,
 });
 
